9889761ac2d216cb6e9411363cec9dc59c27c53e
Lavish Handa <lavish.handa@gmail.com>
2020-05-28 02:34:37 -0400

    fixed stackoverflow error

---
+++ src/Shared/hooks/plain.py
@@ -0,0 +1,2 @@
+dict1 = {"Python":1,"Java":2,"Python":3};
+print(dict1["Python"]);
--- src/places/pages/newplace.js
+++ src/places/pages/newplace.js
@@ -6,35 +6,12 @@ import {
   VALIDATOR_REQUIRE,
   VALIDATOR_MINLENGTH
 } from '../../Shared/Util/validators';
+import {useForm} from '../../Shared/hooks/form-hook';
 import './PlaceForm.css';
 
-const formReducer = (state, action) => {
-  switch (action.type) {
-    case 'INPUT_CHANGE':
-      let formIsValid = true;
-      for (const inputId in state.inputs) {
-        if (inputId === action.inputId) {
-          formIsValid = formIsValid && action.isValid;
-        } else {
-          formIsValid = formIsValid && state.inputs[inputId].isValid;
-        }
-      }
-      return {
-        ...state,
-        inputs: {
-          ...state.inputs,
-          [action.inputId]: { value: action.value, isValid: action.isValid }
-        },
-        isValid: formIsValid
-      };
-    default:
-      return state;
-  }
-};
-
 const NewPlace = () => {
-  const [formState, dispatch] = useReducer(formReducer, {
-    inputs: {
+  const [formState, inputHandler] = useForm(
+    {
       title: {
         value: '',
         isValid: false
@@ -42,19 +19,14 @@ const NewPlace = () => {
       description: {
         value: '',
         isValid: false
+      },
+      address: {
+        value: '',
+        isValid: false
       }
     },
-    isValid: false
-  });
-
-  const inputHandler = useCallback((id, value, isValid) => {
-    dispatch({
-      type: 'INPUT_CHANGE',
-      value: value,
-      isValid: isValid,
-      inputId: id
-    });
-  }, []);
+    false
+  );
 
   const placeSubmitHandler = event => {
     event.preventDefault();
